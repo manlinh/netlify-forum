606945b9c9effaa18669a36ebd5d64ccb8ebe93a
manlinh <135205672+manlinh@users.noreply.github.com>
2025-08-08 16:51:49 +0800

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,1 +1,7 @@
-export { GET, POST } from "@/auth";
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+export const {
+  handlers: { GET, POST },
+  auth, signIn, signOut,
+} = NextAuth(authOptions);
